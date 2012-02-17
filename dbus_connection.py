0a9fcc5e309976038bf5024505e914c935fb0bcd
Daniel Anthofer <daniel-anthofer@web.de>
2012-02-17 11:02:33 +0100

    access to functions to mount, unmount, list devices works

--- dbus_connection.py
+++ dbus_connection.py
@@ -3,44 +3,65 @@
 # GNU GPL v. 3 is permitted
 # http://www.gnu.org/licenses/gpl-3.0.txt
 
+'''createDevList() returns a list of FileSystemDevice instances
+of currently avaliable file systems'''
+
 import dbus
 
-system_bus = dbus.SystemBus()
+# FileSystemDevice(devicefilepath, system_bus) {{{
+class FileSystemDevice:
+	'''attributes:
+		devicefile
+		fstype
+		label
+		size
+		mountpoint
+	methods:
+		mounted
+		mount
+		unmount
+	'''
 
-#find devices
+	def __init__(self, devicefilepath, system_bus):
+		self.filesystemproxy = system_bus.get_object('org.freedesktop.UDisks', devicefilepath)
+		self.filesystempropertiesinterface = dbus.Interface(self.filesystemproxy, 'org.freedesktop.DBus.Properties')
+		self.get_filesystem_property = self.filesystempropertiesinterface.get_dbus_method('Get')
+		if not self.get_filesystem_property('org.freedesktop.UDisks.Device', 'IdUsage') == 'filesystem':
+			raise TypeError('not a filesystem')
+		self.filesystemmethodsinterface = dbus.Interface(self.filesystemproxy, 'org.freedesktop.UDisks.Device')
+
+		self.devicefile = devicefilepath
+		self.fstype = self.get_filesystem_property('org.freedesktop.UDisks.Device', 'IdType')
+		self.label = self.get_filesystem_property('org.freedesktop.UDisks.Device', 'IdLabel')
+		self.size = self.get_filesystem_property('org.freedesktop.UDisks.Device', 'DeviceSize')
+		if self.get_filesystem_property('org.freedesktop.UDisks.Device', 'DeviceIsMounted'):
+			self.mountpoint = str( self.get_filesystem_property('org.freedesktop.UDisks.Device', 'DeviceMountPaths')[0] )
+		else:
+			self.mountpoint = None
+		
+		self.mount_fs = self.filesystemmethodsinterface.get_dbus_method('FilesystemMount')
+		self.unmount_fs = self.filesystemmethodsinterface.get_dbus_method('FilesystemUnmount')
+		
+	def mounted(self):
+		return bool(self.get_filesystem_property('org.freedesktop.UDisks.Device', 'DeviceIsMounted'))
+
+	def mount(self):
+		self.mountpoint = str( self.mount_fs(self.fstype, []) )
+
+	def unmount(self):
+		self.unmount_fs([])
+# }}}
+
+system_bus = dbus.SystemBus()
 proxy = system_bus.get_object('org.freedesktop.UDisks', '/org/freedesktop/UDisks')
 interface = dbus.Interface(proxy, 'org.freedesktop.UDisks')
 list_devices = interface.get_dbus_method('EnumerateDevices')
 
-print(list_devices())
-filesystemproxy = system_bus.get_object('org.freedesktop.UDisks', list_devices()[-1])
-
-#get properties of a device
-filesystempropertiesinterface = dbus.Interface(filesystemproxy, 'org.freedesktop.DBus.Properties')
-get_filesystem_property = filesystempropertiesinterface.get_dbus_method('Get')
-print(get_property('org.freedesktop.UDisks.Device', 'DeviceIsMounted'))
-print(get_property('org.freedesktop.UDisks.Device', 'DeviceIsPartition'))
-print(get_property('org.freedesktop.UDisks.Device', 'DeviceMountPaths'))
-print(get_property('org.freedesktop.UDisks.Device', 'DeviceIsReadOnly'))
-print(get_property('org.freedesktop.UDisks.Device', 'IdUsage')) #filesystem, crypto, partitiontable, raid, other
-
-print(get_property('org.freedesktop.UDisks.Device', 'IdType')) 	#filesystem: filesystemtype (i.e. vfat)
-																																#crypto: cryptotype (i.e. crypto_LUKS)
-																																#...
-
-#unmount a device
-filesystemmethodsinterface = dbus.Interface(filesystemproxy, 'org.freedesktop.UDisks.Device')
-unmount_filesystem = filesystemmethodsinterface.get_dbus_method('FilesystemUnmount')
-try:
-	unmount_filesystem([])#array of option strings, even if it is empty
-												#currently only the option 'force' is accepted
-except dbus.exceptions.DBusException:
-	print("Cannot unmount because file system on device is busy")
-
-#mount a device
-mountpoint = 'nowhere'
-if get_property('org.freedesktop.UDisks.Device', 'IdUsage') == 'filesystem':
-	filesystemtype = get_property('org.freedesktop.UDisks.Device', 'IdType')
-	mount_filesystem = filesystemmethodsinterface.get_dbus_method('FilesystemMount')
-	mountpoint = mount_filesystem(filesystemtype, []) #array of option strings, even if it is empty
-
+def createDevList():
+	devList = []
+	for devfile in list_devices():
+		try:
+			devList.append(FileSystemDevice(devfile, system_bus))
+		except TypeError:
+			pass # skip devicefiles with i.e. partition tables
+	return devList
